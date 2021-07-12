cd14c14de01ab12457bd31dc558d12a4aa72d992
Anxing Xiao <anxingx@uci.edu>
2021-07-13 04:00:20 +0800

    Update clf_cbf_nmpc.py

--- clf_cbf_nmpc.py
+++ clf_cbf_nmpc.py
@@ -8,31 +8,6 @@ from cvxopt import matrix, sparse
 import casadi as ca
 import numpy as np
 
-def Simple_Catcher(attacker_state,defender_state):
-    is_poistive = 1
-    distance = np.sqrt(np.sum(np.square(attacker_state[:2] - defender_state[:2])))
-    dx =attacker_state[0] - defender_state[0]
-    dy =attacker_state[1] - defender_state[1]
-    theta_e = np.arctan2(dy,dx) - defender_state[2]
-    # print(np.arctan(dy/dx))
-    # print(defender_state[2])
-    # attacker_state[2] - defender_state[2]
-    if(theta_e>np.pi):
-        theta_e -= 2*np.pi
-    elif (theta_e<-np.pi):
-        theta_e += 2*np.pi
-    # print(theta_e)
-    
-    if(theta_e>np.pi/2 or theta_e<-np.pi/2):
-        is_poistive = -1
-
-    u = 1.5*distance*is_poistive
-    w = theta_e*is_poistive
-    u = np.clip(u, -0.5, 0.5)
-    w = np.clip(w, -2, 2)
-    return np.array([u,w])
-
-
 def CLF_CBF_NMPC(attacker_state,defender_state):
     
     opti = ca.Opti()
@@ -166,6 +141,33 @@ def Base_NMPC(attacker_state,defender_state):
     u_res = sol.value(opt_controls)
 
     return u_res[0, :]
+
+def Simple_Catcher(attacker_state,defender_state):
+    is_poistive = 1
+    distance = np.sqrt(np.sum(np.square(attacker_state[:2] - defender_state[:2])))
+    dx =attacker_state[0] - defender_state[0]
+    dy =attacker_state[1] - defender_state[1]
+    theta_e = np.arctan2(dy,dx) - defender_state[2]
+    # print(np.arctan(dy/dx))
+    # print(defender_state[2])
+    # attacker_state[2] - defender_state[2]
+    if(theta_e>np.pi):
+        theta_e -= 2*np.pi
+    elif (theta_e<-np.pi):
+        theta_e += 2*np.pi
+    # print(theta_e)
+    
+    if(theta_e>np.pi/2 or theta_e<-np.pi/2):
+        is_poistive = -1
+
+    u = 1.5*distance*is_poistive
+    w = theta_e*is_poistive
+    u = np.clip(u, -0.5, 0.5)
+    w = np.clip(w, -2, 2)
+    return np.array([u,w])
+
+
+
 
         
 
